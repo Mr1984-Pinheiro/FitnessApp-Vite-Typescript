261e3cccc28daf892b7208d9cd49af3bf541befe
Carlos Eduardo Tavares Pinheiro <cadutapinheiro@gmail.com>
2023-03-30 09:10:50 -0300

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/scenes/navbar"
 import { useEffect, useState } from "react"
 import { SelectedPage } from "@/shared/types";
+import Home from "@/scenes/home";
 
 
 function App() { 
@@ -28,6 +29,8 @@ function App() {
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
     />     
+
+    <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }
